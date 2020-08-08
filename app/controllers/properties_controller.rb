cf6b6aef92a001567b2a4571d6d082aa0fe70adb
takayama shunya <katonbo1@icloud.com>
2020-08-08 17:14:39 +0900

    controller コメント削除

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,32 +1,24 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: %i[ show edit update destroy ]
 
-  # GET /properties
-  # GET /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
     2.times { @property.stations.build }
     @n = 0
   end
 
-  # GET /properties/1/edit
   def edit
     @property.stations.build
     @n = 0
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
     if @property.save
@@ -36,8 +28,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     if @property.update(property_params)
       redirect_to @property, notice: 'Property was successfully updated.' 
@@ -46,20 +36,16 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
       redirect_to properties_url, notice: 'Property was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_property
     @property = Property.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def property_params
     params.require(:property).permit(
       :name, :price, :address, :constraction, :remark,
